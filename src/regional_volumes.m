c9fb4ed81d28adaf01ee43cd494b99e8c7e3335f
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-09-17 16:48:55 -0500

    volumes

---
+++ src/regional_volumes.m
@@ -0,0 +1,22 @@
+%function regional_volumes(out_dir,spm_dir)
+
+% Load atlas labels, assuming the Lobules-SUIT atlas
+copyfile([spm_dir '/toolbox/suit/atlasesSUIT/Lobules-SUIT.nii.lut'] ,...
+	[out_dir '/iw_Lobules-SUIT_u_a_t1_seg1-lut.txt']);
+labels = readtable([out_dir '/iw_Lobules-SUIT_u_a_t1_seg1-lut.txt'], ...
+	'Delimiter','space','ReadVariableNames',false);
+labels = labels(:,{'Var1','Var8'});
+labels.Properties.VariableNames = {'Label','Region'};
+
+% Compute volumes for each label
+V = spm_vol([out_dir '/iw_Lobules-SUIT_u_a_t1_seg1.nii']);
+voxvol = abs(det(V.mat(1:3,1:3)));
+Y = spm_read_vols(V);
+for k = 1:height(labels)
+	labels.Voxels(k,1) = sum(Y(:)==labels.Label(k));
+	labels.Volume_mm3(k,1) = round(voxvol * labels.Voxels(k,1), 3);
+end
+
+% Store labels and volumes to file
+writetable(labels,[out_dir '/iw_Lobules-SUIT_u_a_t1_seg1-volumes.csv']);
+
