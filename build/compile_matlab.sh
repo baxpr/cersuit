1f4000a8bd97545614a582a00c2d9107f99095e6
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-09-18 18:46:45 -0500

    Move external

--- build/compile_matlab.sh
+++ build/compile_matlab.sh
@@ -15,7 +15,7 @@
 
 
 # Where to find SPM12 on our compilation machine
-SPM_PATH=/wkdir/src/external/spm12
+SPM_PATH=/wkdir/external/spm12
 
 # We may need to add Matlab to the path on the compilation machine
 export PATH=/usr/local/MATLAB/R2019b/bin:${PATH}
